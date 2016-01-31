1744a0a6abfe86152d0a761b5094097b98b356e1
Idris <idris.maps@gmx.ch>
2016-01-31 16:54:32 +0100

    mouse and touch events

--- main.js
+++ main.js
@@ -1,17 +1,16 @@
 var init = require('./lib/init-canvas')
 var data = require('./data.json')
+var isMobile = require('./lib/is-mobile')
 
 init({}, function(THREE, scene, camera, renderer) {
 
-	// CONTROLS
-
-	var b = document.body
-	var ctrl = document.createElement('IMG')
-	ctrl.style.position = 'fixed'
-	ctrl.style.left = '10px'
-	ctrl.style.top = '10px'
-	ctrl.src='ctrl.png'
-	b.appendChild(ctrl)
+	setTimeout(function() { onWindowResize() },100)
+	window.addEventListener( 'resize', onWindowResize, false )
+	function onWindowResize(){
+		camera.aspect = window.innerWidth / window.innerHeight
+		camera.updateProjectionMatrix()
+		renderer.setSize( window.innerWidth, window.innerHeight )
+	}
 
 	// GEOMETRY
 
@@ -53,24 +52,65 @@ init({}, function(THREE, scene, camera, renderer) {
 	camera.position.x = 0
 	camera.position.y = 0
 
-	document.addEventListener('keydown', function(e) {
-		console.log(e.key)
-		if(e.keyCode === 38) { camera.position.y = camera.position.y + 2 }
-		if(e.keyCode === 40) { camera.position.y = camera.position.y - 2 }
-		if(e.keyCode === 39) { camera.position.x = camera.position.x + 2 }
-		if(e.keyCode === 37) { camera.position.x = camera.position.x - 2 }
-		if(e.keyCode === 81) { camera.position.z = camera.position.z - 2 }
-		if(e.keyCode === 65) { camera.position.z = camera.position.z + 2 }
-		if(e.keyCode === 69) { ch.rotation.x = ch.rotation.x + 0.1 }
-		if(e.keyCode === 68) { ch.rotation.x = ch.rotation.x - 0.1 }
-		if(e.keyCode === 70) { ch.rotation.y = ch.rotation.y + 0.1 }
-		if(e.keyCode === 83) { ch.rotation.y = ch.rotation.y - 0.1 }
-	})
 
+document.ontouchmove = function(event){
+event.preventDefault();}
+
+
+window.swipe = {m: false, x1: null, y1: null, x2: null, y2: null}
+if(isMobile()) {
+	document.addEventListener('touchstart', function(down) {
+		down.preventDefault()
+		window.swipe.m = true
+		window.swipe.x1 = down.touches[0].clientX
+		window.swipe.y1 = down.touches[0].clientY
+		document.addEventListener('touchmove', function(move) {
+			move.preventDefault()
+			window.swipe.x2 = move.touches[0].clientX
+			window.swipe.y2 = move.touches[0].clientY
+		})
+		document.addEventListener('touchend', function(up) {
+			up.preventDefault()
+			window.swipe.m = false
+		})
+	})
+} else {
+	document.addEventListener('mousedown', function(down) {
+		down.preventDefault()
+		window.swipe.m = true
+		window.swipe.x1 = down.clientX
+		window.swipe.y1 = down.clientY
+		document.addEventListener('mousemove', function(move) {
+			move.preventDefault()
+			window.swipe.x2 = move.clientX
+			window.swipe.y2 = move.clientY
+		})
+		document.addEventListener('mouseup', function(up) {
+			up.preventDefault()
+			window.swipe.m = false
+		})
+	})
+	document.addEventListener('wheel', function(wheel) {
+		wheel.preventDefault()
+		camera.position.z = camera.position.z + wheel.deltaY
+	})
+}
+
+
+	document.addEventListener('touchstart', function(e) { console.log(e) })
 
 	// RENDER
 
 	function render() {
+		var swipe = window.swipe
+		if(swipe.m === true) {
+			var xMove = swipe.x2 - swipe.x1
+			var yMove = swipe.y2 - swipe.y1
+			ch.rotation.y = ch.rotation.y + xMove * 0.005
+			ch.rotation.x = ch.rotation.x + yMove * 0.005
+			window.swipe.x1 = swipe.x2
+			window.swipe.y1 = swipe.y2
+		}
 		requestAnimationFrame( render )
 		renderer.render( scene, camera );
 	}
